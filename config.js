699ab844dc48571c58328286acdc6dad1cbfdc08
Novaxux <manuelova@outlook.es>
2025-04-30 18:27:33 -0600

    enviroment config changed

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-const IP = process.env.IP || '127.0.0.1';
-const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS) || 10;
+const PORT = process.env.PORT;
+const IP = process.env.IP;
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
 
 export { PORT, IP, SALT_ROUNDS };
